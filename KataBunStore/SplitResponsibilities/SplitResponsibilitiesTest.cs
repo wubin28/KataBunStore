ccece404c36481415e8b22aace340d22f1761f4d
Ben Wu <wubin28@gmail.com>
2013-12-09 21:36:42 +0800

    Moved the class Bun to another file.

--- KataBunStore/SplitResponsibilities/SplitResponsibilitiesTest.cs
+++ KataBunStore/SplitResponsibilities/SplitResponsibilitiesTest.cs
@@ -54,8 +54,4 @@ namespace SplitResponsibilities
                 , "Failure - the output was not 'Mixed stuffing of Su Sam Sun bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.'");
         }
     }
-
-    public class Bun
-    {
-    }
 }
