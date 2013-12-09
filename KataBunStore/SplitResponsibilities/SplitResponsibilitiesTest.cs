ffc58ba4263e1f8b6230b8eb5aff9f438dc64486
Ben Wu <wubin28@gmail.com>
2013-12-09 20:44:57 +0800

    Added the test TestOrderSamSunAndPorkBun().

--- KataBunStore/SplitResponsibilities/SplitResponsibilitiesTest.cs
+++ KataBunStore/SplitResponsibilities/SplitResponsibilitiesTest.cs
@@ -18,5 +18,18 @@ namespace SplitResponsibilities
             // Then
             Assert.AreEqual("Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.", splitResponsibilitiesTestFacade.getOutputForTest(), "Failure - the output was not 'Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.'");
         }
+
+        [Test]
+        public void TestOrderSamSunAndPorkBun()
+        {
+            // Given
+            SplitResponsibilitiesTestFacade splitResponsibilitiesTestFacade = new SplitResponsibilitiesTestFacade();
+
+            // When
+            splitResponsibilitiesTestFacade.orderBun("Sam Sun and Pork");
+
+            // Then
+            Assert.AreEqual("Mixed stuffing of Sam Sum and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.", splitResponsibilitiesTestFacade.getOutputForTest(), "Failure - the output was not 'Mixed stuffing of Sam Sum and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.'");
+        }
     }
 }
