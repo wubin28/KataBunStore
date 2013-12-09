e452088c5e3c958a99040ef2f2f3d107f9b28fbc
Ben Wu <wubin28@gmail.com>
2013-12-09 21:05:29 +0800

    Made the test TestOrderSamSunAndPorkBun() green with bad code.

--- KataBunStore/SplitResponsibilities/SplitResponsibilitiesTestFacade.cs
+++ KataBunStore/SplitResponsibilities/SplitResponsibilitiesTestFacade.cs
@@ -2,14 +2,26 @@
 {
     public class SplitResponsibilitiesTestFacade
     {
+        private string outputForTest = "";
+
         public void orderBun(string bunName)
         {
+            if (bunName == "Green Onion and Pork")
+            {
+                outputForTest =
+                    "Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.";
+            }
+            else
+            {
+                outputForTest =
+                    "Mixed stuffing of Sam Sum and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.";
+            }
             
         }
 
         public string getOutputForTest()
         {
-            return "Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.";
+            return outputForTest;
         }
     }
 }
