86108a52b7f1294e2f6b15dd56f69a06bf933814
Ben Wu <wubin28@gmail.com>
2013-12-09 14:10:15 +0800

    Changed to NUnit.

--- KataBunStore/SplitResponsibilities/UnitTest1.cs
+++ KataBunStore/SplitResponsibilities/UnitTest1.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
+using System;
 
 namespace SplitResponsibilities
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
+        [Test]
         public void TestMethod1()
         {
         }
