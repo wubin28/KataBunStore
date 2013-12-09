b705131e687c12ecb0755e6e4d332b70841755fa
Ben Wu <wubin28@gmail.com>
2013-12-09 21:42:36 +0800

    Made the expected string to be the parameter of the constructor of class Bun.

--- KataBunStore/SplitResponsibilities/SplitResponsibilitiesTestFacade.cs
+++ KataBunStore/SplitResponsibilities/SplitResponsibilitiesTestFacade.cs
@@ -2,24 +2,20 @@
 {
     public class SplitResponsibilitiesTestFacade
     {
-        private string outputForTest = "";
 
         public Bun orderBun(string bunName)
         {
             if (bunName == "Green Onion and Pork")
             {
-                outputForTest =
-                    "Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.";
+                return new Bun("Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
             }
             else if (bunName == "Sam Sun and Pork")
             {
-                outputForTest =
-                    "Mixed stuffing of Sam Sun and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.";
+                return new Bun("Mixed stuffing of Sam Sun and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
             }
             else
             {
-                outputForTest =
-                    "Mixed stuffing of Su Sam Sun bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.";
+                return new Bun("Mixed stuffing of Su Sam Sun bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
             }
             
         }
