f85d07d21595232ae5f1f0890820e06726d1d2c3
Ben Wu <wubin28@gmail.com>
2013-12-10 20:49:51 +0800

    Created class SamSunAndPorkBun and moved it to another class.

---
+++ KataBunStore/SplitResponsibilities/SamSunAndPorkBun.cs
@@ -0,0 +1,10 @@
+﻿namespace SplitResponsibilities
+{
+    public class SamSunAndPorkBun : Bun
+    {
+        public SamSunAndPorkBun(string outputForTest) : base (outputForTest)
+        {
+            
+        }
+    }
+}
