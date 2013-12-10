b6d55c36aa52456ec3fe8beed7d08dfc52647046
Ben Wu <wubin28@gmail.com>
2013-12-10 19:15:02 +0800

    Wrote constructor of class Waiter.

--- KataBunStore/SplitResponsibilities/Waiter.cs
+++ KataBunStore/SplitResponsibilities/Waiter.cs
@@ -2,10 +2,13 @@
 {
     public class Waiter
     {
+        private Cook cook;
+
         public Waiter(Cook cook)
         {
-            
+            this.cook = cook;
         }
+
 
         public Bun dishOut(string bunName)
         {
