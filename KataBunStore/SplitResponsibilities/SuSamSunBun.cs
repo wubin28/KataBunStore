5e8da973e94e091e80e28675575ceaa299e7cf08
Ben Wu <wubin28@gmail.com>
2013-12-10 20:51:37 +0800

    Created class SuSamSunBun and moved to another file.

---
+++ KataBunStore/SplitResponsibilities/SuSamSunBun.cs
@@ -0,0 +1,10 @@
+﻿namespace SplitResponsibilities
+{
+    public class SuSamSunBun : Bun
+    {
+        public SuSamSunBun(string outputForTest) : base (outputForTest)
+        {
+            
+        }
+    }
+}
