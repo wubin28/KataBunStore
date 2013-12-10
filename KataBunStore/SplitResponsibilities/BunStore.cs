1976a4c32c02423ff4c28be02b51abfeb35009ca
Ben Wu <wubin28@gmail.com>
2013-12-10 15:00:03 +0800

    Created method BunStore.orderBun() and made all tests green.

--- KataBunStore/SplitResponsibilities/BunStore.cs
+++ KataBunStore/SplitResponsibilities/BunStore.cs
@@ -2,5 +2,20 @@
 {
     public class BunStore
     {
+        public Bun orderBun(string bunName)
+        {
+            if (bunName == "Green Onion and Pork")
+            {
+                return new Bun("Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
+            }
+            else if (bunName == "Sam Sun and Pork")
+            {
+                return new Bun("Mixed stuffing of Sam Sun and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
+            }
+            else
+            {
+                return new Bun("Mixed stuffing of Su Sam Sun bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
+            }
+        }
     }
 }
--- KataBunStore/SplitResponsibilities/SplitResponsibilitiesTestFacade.cs
+++ KataBunStore/SplitResponsibilities/SplitResponsibilitiesTestFacade.cs
@@ -7,18 +7,6 @@
         {
             BunStore bunStore = new BunStore();
             return bunStore.orderBun(bunName);
-            if (bunName == "Green Onion and Pork")
-            {
-                return new Bun("Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
-            }
-            else if (bunName == "Sam Sun and Pork")
-            {
-                return new Bun("Mixed stuffing of Sam Sun and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
-            }
-            else
-            {
-                return new Bun("Mixed stuffing of Su Sam Sun bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
-            }
             
         }
 
