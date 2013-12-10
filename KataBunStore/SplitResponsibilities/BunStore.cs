87911f168839174923488173f3160948ff471feb
Ben Wu <wubin28@gmail.com>
2013-12-10 19:40:15 +0800

    Moved the bun object instantiation logic to class Cook.

--- KataBunStore/SplitResponsibilities/BunStore.cs
+++ KataBunStore/SplitResponsibilities/BunStore.cs
@@ -8,18 +8,6 @@
             Waiter waiter = new Waiter(cook);
             return waiter.dishOut(bunName);
 
-            if (bunName == "Green Onion and Pork")
-            {
-                return new Bun("Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
-            }
-            else if (bunName == "Sam Sun and Pork")
-            {
-                return new Bun("Mixed stuffing of Sam Sun and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
-            }
-            else
-            {
-                return new Bun("Mixed stuffing of Su Sam Sun bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
-            }
         }
     }
 }
