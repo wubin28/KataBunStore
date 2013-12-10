3128dd2b19d171eb54f44db7e669b9cc556fe3f9
Ben Wu <wubin28@gmail.com>
2013-12-10 20:42:25 +0800

    Returned instances of subclasses of Bun.

--- KataBunStore/SplitResponsibilities/Cook.cs
+++ KataBunStore/SplitResponsibilities/Cook.cs
@@ -6,15 +6,15 @@
         {
             if (bunName == "Green Onion and Pork")
             {
-                return new Bun("Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
+                return new GreenOnionAndPorkBun("Mixed stuffing of Green Onion and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
             }
             else if (bunName == "Sam Sun and Pork")
             {
-                return new Bun("Mixed stuffing of Sam Sun and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
+                return new SamSunAndPorkBun("Mixed stuffing of Sam Sun and Pork bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
             }
             else
             {
-                return new Bun("Mixed stuffing of Su Sam Sun bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
+                return new SuSamSunBun("Mixed stuffing of Su Sam Sun bun. Kneaded into dough. Wrapped buns. Steamed buns. Dished out buns.");
             }
         }
     }
