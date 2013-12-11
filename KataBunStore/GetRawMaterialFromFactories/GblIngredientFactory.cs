d91401297dd52f718eb21b80e494265cc968c255
Ben Wu <wubin28@gmail.com>
2013-12-11 23:22:50 +0800

    Created class GblIngredientFactory.

---
+++ KataBunStore/GetRawMaterialFromFactories/GblIngredientFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace GetRawMaterialFromFactories
+{
+    public class GblIngredientFactory : BunIngredientFactory
+    {
+        public override Stuffing CreateStuffing()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Flour CreateFlour()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
