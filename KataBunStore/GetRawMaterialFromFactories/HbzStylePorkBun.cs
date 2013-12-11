ab89398dadac384c8089b57332bbf772080ebf72
Ben Wu <wubin28@gmail.com>
2013-12-12 00:55:13 +0800

    Made stuffing and flour created by the HBZ ingredient factory and appended the step description in the constructor of class HbzStylePorkBun.

--- KataBunStore/GetRawMaterialFromFactories/HbzStylePorkBun.cs
+++ KataBunStore/GetRawMaterialFromFactories/HbzStylePorkBun.cs
@@ -8,6 +8,11 @@
         {
             this.hbzIngredientFactory = hbzIngredientFactory;
 
+            Stuffing = hbzIngredientFactory.CreateStuffing();
+            stepDesc += Stuffing.GetDesc();
+
+            Flour = hbzIngredientFactory.CreateFlour();
+            stepDesc += Flour.GetDesc();
         }
 
         public override string GetStepDesc()
