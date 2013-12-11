0608e94998cd4b71634961bd6f00a21c02dbeaf1
Ben Wu <wubin28@gmail.com>
2013-12-11 12:48:22 +0800

    Wrote assertion for the 2nd test TestOrderGblStylePorkBun().

--- KataBunStore/GetRawMaterialFromFactories/GetRawMaterialFromFactoriesTest.cs
+++ KataBunStore/GetRawMaterialFromFactories/GetRawMaterialFromFactoriesTest.cs
@@ -21,5 +21,14 @@ namespace GetRawMaterialFromFactories
                 , bun.GetStepDesc()
                 , "Failure - the step description was not 'Got mixed stuffing of Green Onion and Pork bun from QF Bun Factory. Got flour from QF Bun Factory. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.'");
         }
+
+        [Test]
+        public void TestOrderGblStylePorkBun()
+        {
+            // Then
+            Assert.AreEqual("Got mixed stuffing of Pork bun in GBL style. Got flour from GBL Bun Factory. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns."
+                , bun.GetStepDesc()
+                , "Failure - the step description was not 'Got mixed stuffing of Pork bun in GBL style. Got flour from GBL Bun Factory. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.'");
+        }
     }
 }
