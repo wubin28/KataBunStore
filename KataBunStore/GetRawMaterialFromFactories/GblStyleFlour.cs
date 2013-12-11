6329197eb407a96f533cc1da3d235dd29121e695
Ben Wu <wubin28@gmail.com>
2013-12-11 23:27:38 +0800

    Created class GblStyleFlour.

---
+++ KataBunStore/GetRawMaterialFromFactories/GblStyleFlour.cs
@@ -0,0 +1,10 @@
+﻿namespace GetRawMaterialFromFactories
+{
+    public class GblStyleFlour : Flour
+    {
+        public override string GetDesc()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
