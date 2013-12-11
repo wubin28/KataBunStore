15b8b4a52ce08879f560528e1cba26c85fdcab80
Ben Wu <wubin28@gmail.com>
2013-12-11 13:46:06 +0800

    Declared the three abstract methods in class Cook: PrepareStuffing(), KneadIntoDough(), WrapBun().

--- KataBunStore/GetRawMaterialFromFactories/Cook.cs
+++ KataBunStore/GetRawMaterialFromFactories/Cook.cs
@@ -7,10 +7,17 @@
         public Bun MakeBun(string bunName)
         {
             Bun bun = CreateBun(bunName);
-            prepareStuffing(bun, bunName);
+            PrepareStuffing(bun, bunName);
             KneadIntoDough(bun);
             WrapBun(bun);
             return bun;
         }
+
+        protected abstract void PrepareStuffing(Bun bun, string bunName);
+
+        protected abstract void KneadIntoDough(Bun bun);
+
+        protected abstract void WrapBun(Bun bun);
+
     }
 }
