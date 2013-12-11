d4f322b69353996ed49244a3c9749b5f1cff2165
Ben Wu <wubin28@gmail.com>
2013-12-12 00:45:47 +0800

    Appended step description in the constructor of class HbzStyleFlour.

--- KataBunStore/GetRawMaterialFromFactories/HbzStyleFlour.cs
+++ KataBunStore/GetRawMaterialFromFactories/HbzStyleFlour.cs
@@ -2,6 +2,10 @@
 {
     public class HbzStyleFlour : Flour
     {
+        public HbzStyleFlour()
+        {
+            Desc += " Got flour from HBZ Ingredient Factory.";
+        }
         public override string GetDesc()
         {
             return Desc;
