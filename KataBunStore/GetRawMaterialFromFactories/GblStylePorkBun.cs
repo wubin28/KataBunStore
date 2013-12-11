72879fc2a8ab04b4084df5e356729b4bb1eb4bb9
Ben Wu <wubin28@gmail.com>
2013-12-11 23:39:36 +0800

    Created constructor of class GblStylePorkBun with a GBL Ingredient Factory parameter.

--- KataBunStore/GetRawMaterialFromFactories/GblStylePorkBun.cs
+++ KataBunStore/GetRawMaterialFromFactories/GblStylePorkBun.cs
@@ -2,6 +2,13 @@
 {
     public class GblStylePorkBun : Bun
     {
+        private BunIngredientFactory gblIngredientFactory;
+
+        public GblStylePorkBun(BunIngredientFactory gblIngredientFactory)
+        {
+            this.gblIngredientFactory = gblIngredientFactory;
+        }
+
         public override string GetStepDesc()
         {
             return "Got mixed stuffing of Pork bun from GBL Ingredient Factory. Got flour from GBL Ingredient Factory. Prepared stuffing. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.";
