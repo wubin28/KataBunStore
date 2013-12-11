a477d8517a4a91f77765b676bb4e9f5d38543193
Ben Wu <wubin28@gmail.com>
2013-12-11 09:48:25 +0800

    Created a QfStyleCook with the Cook interface.

--- KataBunStore/AddTwoNewFlavors/BunStore.cs
+++ KataBunStore/AddTwoNewFlavors/BunStore.cs
@@ -4,6 +4,7 @@
     {
         public Bun orderQfStyleBun(string bunName)
         {
+            Cook qfStyleCook = new QfStyleCook();
             Waiter waiter = new Waiter(qfStyleCook);
             return waiter.dishOut(bunName);
         }
