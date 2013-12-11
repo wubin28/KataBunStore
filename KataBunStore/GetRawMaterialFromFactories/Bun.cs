f4c02ae77f5692aeaa0a5de2604f3efd50fdb322
Ben Wu <wubin28@gmail.com>
2013-12-11 12:44:38 +0800

    Added method Bun.GetStepDesc() to make the first test green.

--- KataBunStore/GetRawMaterialFromFactories/Bun.cs
+++ KataBunStore/GetRawMaterialFromFactories/Bun.cs
@@ -2,5 +2,9 @@
 {
     public class Bun
     {
+        public string GetStepDesc()
+        {
+            return "Got mixed stuffing of Green Onion and Pork bun from QF Bun Factory. Got flour from QF Bun Factory. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.";
+        }
     }
 }
--- KataBunStore/GetRawMaterialFromFactories/GetRawMaterialFromFactoriesTest.cs
+++ KataBunStore/GetRawMaterialFromFactories/GetRawMaterialFromFactoriesTest.cs
@@ -18,7 +18,7 @@ namespace GetRawMaterialFromFactories
 
             // Then
             Assert.AreEqual("Got mixed stuffing of Green Onion and Pork bun from QF Bun Factory. Got flour from QF Bun Factory. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns."
-                , bun.getStepDesc()
+                , bun.GetStepDesc()
                 , "Failure - the step description was not 'Got mixed stuffing of Green Onion and Pork bun from QF Bun Factory. Got flour from QF Bun Factory. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.'");
         }
     }
