79061ea3ea6030ccc02fab825518c711ac278de6
Ben Wu <wubin28@gmail.com>
2013-12-11 19:00:07 +0800

    Updated the final step descriptions for the two tests: TestOrderQfStyleGreenOnionAndPorkBun() and TestOrderGblStylePorkBun().

--- KataBunStore/GetRawMaterialFromFactories/GetRawMaterialFromFactoriesTest.cs
+++ KataBunStore/GetRawMaterialFromFactories/GetRawMaterialFromFactoriesTest.cs
@@ -17,9 +17,9 @@ namespace GetRawMaterialFromFactories
             Bun bun = bunStore.OrderQfStyleBun("Green Onion and Pork");
 
             // Then
-            Assert.AreEqual("Got mixed stuffing of Green Onion and Pork bun from QF Bun Factory. Got flour from QF Bun Factory. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns."
+            Assert.AreEqual("Got mixed stuffing of Green Onion and Pork bun from QF Ingredient Factory. Got flour from QF Ingredient Factory. Prepared stuffing. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns."
                 , bun.GetStepDesc()
-                , "Failure - the step description was not 'Got mixed stuffing of Green Onion and Pork bun from QF Bun Factory. Got flour from QF Bun Factory. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.'");
+                , "Failure - the step description was not 'Got mixed stuffing of Green Onion and Pork bun from QF Ingredient Factory. Got flour from QF Ingredient Factory. Prepared stuffing. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.'");
         }
 
         [Test]
@@ -32,9 +32,9 @@ namespace GetRawMaterialFromFactories
             Bun bun = bunStore.OrderGblStyleBun("Pork");
 
             // Then
-            Assert.AreEqual("Got mixed stuffing of Pork bun in GBL style. Got flour from GBL Bun Factory. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns."
+            Assert.AreEqual("Got mixed stuffing of Pork bun from GBL Ingredient Factory. Got flour from GBL Ingredient Factory. Prepared stuffing. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns."
                 , bun.GetStepDesc()
-                , "Failure - the step description was not 'Got mixed stuffing of Pork bun in GBL style. Got flour from GBL Bun Factory. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.'");
+                , "Failure - the step description was not 'Got mixed stuffing of Pork bun from GBL Ingredient Factory. Got flour from GBL Ingredient Factory. Prepared stuffing. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.'");
         }
     }
 }
