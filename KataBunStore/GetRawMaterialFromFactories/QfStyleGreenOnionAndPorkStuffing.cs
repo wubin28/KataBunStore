30c6c539e1eb906c69b195fe7d9ca6e6fbec402d
Ben Wu <wubin28@gmail.com>
2013-12-11 20:09:55 +0800

    Wrote description in the default constructor of class QfStyleGreenOnionAndPorkStuffing.

--- KataBunStore/GetRawMaterialFromFactories/QfStyleGreenOnionAndPorkStuffing.cs
+++ KataBunStore/GetRawMaterialFromFactories/QfStyleGreenOnionAndPorkStuffing.cs
@@ -2,6 +2,10 @@
 {
     public class QfStyleGreenOnionAndPorkStuffing : Stuffing
     {
+        public QfStyleGreenOnionAndPorkStuffing()
+        {
+            Desc += "Got mixed stuffing of Green Onion and Pork bun from QF Ingredient Factory.";
+        }
         public override string GetDesc()
         {
             return Desc;
