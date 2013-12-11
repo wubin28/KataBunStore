759674a2020eafdf2500476b9f20d248f487fd10
Ben Wu <wubin28@gmail.com>
2013-12-11 09:14:31 +0800

    Added the Then for the test TestOrderGblStylePorkBun.

--- KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
+++ KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
@@ -21,5 +21,14 @@ namespace AddTwoNewFlavors
                 , bun.getStepDesc()
                 , "Failure - the output was not 'Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.'");
         }
+
+        [Test]
+        public void TestOrderGblStylePorkBun()
+        {
+            // Then
+            Assert.AreEqual("Mixed stuffing of Pork bun in GBL style. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns."
+                , bun.getStepDesc()
+                , "Failure - the output was not 'Mixed stuffing of Pork bun in GBL style. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.'");
+        }
     }
 }
