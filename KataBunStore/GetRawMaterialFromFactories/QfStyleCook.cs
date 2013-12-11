dc23f64c672b94f607ccd1056d9516afc18b619f
Ben Wu <wubin28@gmail.com>
2013-12-11 13:56:29 +0800

    In method QfStyleCook.CreateBun(), the QF style Green Onion and Pork bun should be created with a QF Ingredient factory.

--- KataBunStore/GetRawMaterialFromFactories/QfStyleCook.cs
+++ KataBunStore/GetRawMaterialFromFactories/QfStyleCook.cs
@@ -1,6 +1,34 @@
-﻿namespace GetRawMaterialFromFactories
+﻿using System.IO;
+
+namespace GetRawMaterialFromFactories
 {
     public class QfStyleCook : Cook
     {
+        protected override Bun CreateBun(string bunName)
+        {
+            if (bunName == "Green Onion and Pork")
+            {
+                return new QfStyleGreenOnionAndPorkBun(qfIngredientFactory);
+            }
+            else
+            {
+                throw new InvalidDataException("Invalid bun name: " + bunName);
+            }
+        }
+
+        protected override void PrepareStuffing(Bun bun, string bunName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void KneadIntoDough(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void WrapBun(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
