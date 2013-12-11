3700d667ebb8add0151e54ddb653a55ca97e65b7
Ben Wu <wubin28@gmail.com>
2013-12-12 00:20:53 +0800

    Created class HbzIngredientFactory.

---
+++ KataBunStore/GetRawMaterialFromFactories/HbzIngredientFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace GetRawMaterialFromFactories
+{
+    public class HbzIngredientFactory : BunIngredientFactory
+    {
+        public override Stuffing CreateStuffing()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Flour CreateFlour()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
