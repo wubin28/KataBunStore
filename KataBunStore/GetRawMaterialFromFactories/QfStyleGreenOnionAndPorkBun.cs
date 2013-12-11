b4114d1be20672dcb1825c7e3d6f0d3bcb0c0aa1
Ben Wu <wubin28@gmail.com>
2013-12-11 14:04:46 +0800

    Added constructor of class QfStyleGreenOnionAndPorkBun with a parameter of bun ingredient factory.

--- KataBunStore/GetRawMaterialFromFactories/QfStyleGreenOnionAndPorkBun.cs
+++ KataBunStore/GetRawMaterialFromFactories/QfStyleGreenOnionAndPorkBun.cs
@@ -2,6 +2,13 @@
 {
     public class QfStyleGreenOnionAndPorkBun : Bun
     {
+        private BunIngredientFactory _bunIngredientFactory;
+
+        public QfStyleGreenOnionAndPorkBun(BunIngredientFactory bunIngredientFactory)
+        {
+            this._bunIngredientFactory = bunIngredientFactory;
+        }
+
         public override string GetStepDesc()
         {
             return "Got mixed stuffing of Green Onion and Pork bun from QF Bun Factory. Got flour from QF Bun Factory. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.";
