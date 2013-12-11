9e6602c4bdbd85d6c53125d09957b2876ebc6ea9
Ben Wu <wubin28@gmail.com>
2013-12-12 00:13:37 +0800

    Created class HbzStyleCook.

---
+++ KataBunStore/GetRawMaterialFromFactories/HbzStyleCook.cs
@@ -0,0 +1,25 @@
+﻿namespace GetRawMaterialFromFactories
+{
+    public class HbzStyleCook : Cook
+    {
+        protected override Bun CreateBun(string bunName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void PrepareStuffing(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void KneadIntoDough(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void WrapBun(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
