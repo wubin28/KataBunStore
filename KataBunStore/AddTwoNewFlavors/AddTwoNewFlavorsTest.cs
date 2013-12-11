618f42325e8c940f759fdae4b409b5b41cec2e40
Ben Wu <wubin28@gmail.com>
2013-12-11 09:01:30 +0800

    Wrote the Then of the test first.

--- KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
+++ KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
@@ -10,6 +10,10 @@ namespace AddTwoNewFlavors
         [Test]
         public void TestOrderQfStyleGreenOnionAndPorkBun()
         {
+            // Then
+            Assert.AreEqual("Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns."
+                , bun.getStepDesc("Green Onion and Pork")
+                , "Failure - the output was not 'Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.'");
         }
     }
 }
