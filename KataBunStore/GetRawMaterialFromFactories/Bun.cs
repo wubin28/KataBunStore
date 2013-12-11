6a7011fbbe0607ceba091b1fdca90ec2009291c4
Ben Wu <wubin28@gmail.com>
2013-12-11 13:03:13 +0800

    Made class Bun to be abstract and the method GetStepDesc() to be abstract as well. Will ask the subclasses of Bun to return specific step description.

--- KataBunStore/GetRawMaterialFromFactories/Bun.cs
+++ KataBunStore/GetRawMaterialFromFactories/Bun.cs
@@ -1,10 +1,7 @@
 ﻿namespace GetRawMaterialFromFactories
 {
-    public class Bun
+    public abstract class Bun
     {
-        public string GetStepDesc()
-        {
-            return "Got mixed stuffing of Green Onion and Pork bun from QF Bun Factory. Got flour from QF Bun Factory. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.";
-        }
+        public abstract string GetStepDesc();
     }
 }
