adada192a4b8c4d33c788ee1c5d843b06e6afaaa
Ben Wu <wubin28@gmail.com>
2013-12-11 11:48:20 +0800

    Added class HbzStyleCook and moved to another file.

---
+++ KataBunStore/AddTwoNewFlavors/HbzStyleCook.cs
@@ -0,0 +1,25 @@
+﻿namespace AddTwoNewFlavors
+{
+    public class HbzStyleCook : Cook
+    {
+        protected override Bun CreateBun(string bunName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void WrapBun(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void KneadIntoDough(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void MixStuffing(Bun bun, string bunName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
