492af41c34a50a8465c483cd6b42f2a06fbade4f
Ben Wu <wubin28@gmail.com>
2013-12-11 23:46:46 +0800

    The method GblStylePorkBun.GetStepDesc() returned the accumulated step description.

--- KataBunStore/GetRawMaterialFromFactories/GblStylePorkBun.cs
+++ KataBunStore/GetRawMaterialFromFactories/GblStylePorkBun.cs
@@ -17,7 +17,7 @@
 
         public override string GetStepDesc()
         {
-            return "Got mixed stuffing of Pork bun from GBL Ingredient Factory. Got flour from GBL Ingredient Factory. Prepared stuffing. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.";
+            return stepDesc;
         }
     }
 }
