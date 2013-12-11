8bf35e97d139eadaec81b23512e8ea049442e725
Ben Wu <wubin28@gmail.com>
2013-12-11 23:35:46 +0800

    Appended step description in the constructor of class GblStyleFlour.

--- KataBunStore/GetRawMaterialFromFactories/GblStyleFlour.cs
+++ KataBunStore/GetRawMaterialFromFactories/GblStyleFlour.cs
@@ -2,6 +2,10 @@
 {
     public class GblStyleFlour : Flour
     {
+        public GblStyleFlour()
+        {
+            Desc += " Got flour from GBL Ingredient Factory.";
+        }
         public override string GetDesc()
         {
             return Desc;
