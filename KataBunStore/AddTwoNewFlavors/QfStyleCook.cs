db4f2ba7cc22660d0a93bb8d89b7a6a465c04971
Ben Wu <wubin28@gmail.com>
2013-12-11 10:34:21 +0800

    Implemented method QfStyleCook.CreateBun().

--- KataBunStore/AddTwoNewFlavors/QfStyleCook.cs
+++ KataBunStore/AddTwoNewFlavors/QfStyleCook.cs
@@ -1,6 +1,34 @@
-﻿namespace AddTwoNewFlavors
+﻿using System.IO;
+
+namespace AddTwoNewFlavors
 {
     public class QfStyleCook : Cook
     {
+        protected override Bun CreateBun(string bunName)
+        {
+            if (bunName == "Green Onion and Pork")
+            {
+                return new QfStyleGreenOnionAndPorkBun();
+            }
+            else
+            {
+                throw new InvalidDataException("Invalid bun name: " + bunName);
+            }
+        }
+
+        protected override void WrapBun(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void KneadIntoDough(Bun bun)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void MixStuffing(Bun bun, string bunName)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
