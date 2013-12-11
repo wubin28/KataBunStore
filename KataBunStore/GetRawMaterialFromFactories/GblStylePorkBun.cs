b33c367731c50eb467bb1a7371cd261ea4f6ed7c
Ben Wu <wubin28@gmail.com>
2013-12-11 23:44:25 +0800

    In the constructor of class GblStylePorkBun, the stuffing and flour were created by the GBL Ingredient Factory.

--- KataBunStore/GetRawMaterialFromFactories/GblStylePorkBun.cs
+++ KataBunStore/GetRawMaterialFromFactories/GblStylePorkBun.cs
@@ -7,6 +7,12 @@
         public GblStylePorkBun(BunIngredientFactory gblIngredientFactory)
         {
             this.gblIngredientFactory = gblIngredientFactory;
+
+            Stuffing = this.gblIngredientFactory.CreateStuffing();
+            stepDesc += Stuffing.GetDesc();
+
+            Flour = this.gblIngredientFactory.CreateFlour();
+            stepDesc += Flour.GetDesc();
         }
 
         public override string GetStepDesc()
