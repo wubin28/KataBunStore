32b789928531bcef6538dcdd1720cc8d0110738e
Ben Wu <wubin28@gmail.com>
2013-12-11 09:11:10 +0800

    Added method Bun.getStepDesc().

--- KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
+++ KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
@@ -18,7 +18,7 @@ namespace AddTwoNewFlavors
 
             // Then
             Assert.AreEqual("Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns."
-                , bun.getStepDesc("Green Onion and Pork")
+                , bun.getStepDesc()
                 , "Failure - the output was not 'Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.'");
         }
     }
--- KataBunStore/AddTwoNewFlavors/Bun.cs
+++ KataBunStore/AddTwoNewFlavors/Bun.cs
@@ -2,5 +2,9 @@
 {
     public class Bun
     {
+        public string getStepDesc()
+        {
+            return "Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.";
+        }
     }
 }
