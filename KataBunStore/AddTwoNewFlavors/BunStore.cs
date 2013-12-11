c2c8da2d2186dc08a6e5fc67166620f11c04ec0d
Ben Wu <wubin28@gmail.com>
2013-12-11 11:18:00 +0800

    In the method BunStore.orderGblStyleBun(), the waiter should be created with a GBL style cook.

--- KataBunStore/AddTwoNewFlavors/BunStore.cs
+++ KataBunStore/AddTwoNewFlavors/BunStore.cs
@@ -11,7 +11,7 @@
 
         public Bun orderGblStyleBun(string bunName)
         {
-
+            Waiter waiter = new Waiter(gblStyleCook);
             return waiter.DishOut(bunName);
         }
     }
