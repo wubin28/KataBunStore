7636e7d44f2e3d4269d0ab943016fbe2703a011a
Ben Wu <wubin28@gmail.com>
2013-12-11 22:28:47 +0800

    Appended step description in the constructor of class QfStyleFlour.

--- KataBunStore/GetRawMaterialFromFactories/QfStyleFlour.cs
+++ KataBunStore/GetRawMaterialFromFactories/QfStyleFlour.cs
@@ -2,6 +2,10 @@
 {
     public class QfStyleFlour : Flour
     {
+        public QfStyleFlour()
+        {
+            Desc += " Got flour from QF Ingredient Factory.";
+        }
         public override string GetDesc()
         {
             return Desc;
