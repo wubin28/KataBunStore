dad9f78860bac53282cdc78fb32f85f3727dae8f
Ben Wu <wubin28@gmail.com>
2013-12-11 13:06:30 +0800

    Made method GetStepDesc() of class GblStylePorkBun to return its specific step description.

--- KataBunStore/GetRawMaterialFromFactories/GblStylePorkBun.cs
+++ KataBunStore/GetRawMaterialFromFactories/GblStylePorkBun.cs
@@ -2,5 +2,9 @@
 {
     public class GblStylePorkBun : Bun
     {
+        public override string GetStepDesc()
+        {
+            return "Got mixed stuffing of Pork bun in GBL style. Got flour from GBL Bun Factory. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.";
+        }
     }
 }
