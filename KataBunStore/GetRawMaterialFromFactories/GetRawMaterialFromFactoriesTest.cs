95b45ba4381d4e138fd6975b074e5ab902cf925f
Ben Wu <wubin28@gmail.com>
2013-12-11 12:22:30 +0800

    Renamed the first test to be TestOrderQfStyleGreenOnionAndPorkBun().

--- KataBunStore/GetRawMaterialFromFactories/GetRawMaterialFromFactoriesTest.cs
+++ KataBunStore/GetRawMaterialFromFactories/GetRawMaterialFromFactoriesTest.cs
@@ -8,7 +8,7 @@ namespace GetRawMaterialFromFactories
     public class GetRawMaterialFromFactoriesTest
     {
         [Test]
-        public void TestMethod1()
+        public void TestOrderQfStyleGreenOnionAndPorkBun()
         {
         }
     }
