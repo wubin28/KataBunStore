6a048b561644f4cc2e6331642504374ecdcf7868
Ben Wu <wubin28@gmail.com>
2013-12-11 11:37:09 +0800

    Added the assertion for the test of ordering HBZ style Pork bun.

--- KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
+++ KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
@@ -36,5 +36,14 @@ namespace AddTwoNewFlavors
                 , bun.getStepDesc()
                 , "Failure - the output was not 'Mixed stuffing of Pork bun in GBL style. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.'");
         }
+
+        [Test]
+        public void TestOrderHbzStylePorkBun()
+        {
+            // Then
+            Assert.AreEqual("Mixed stuffing of Pork bun in HBZ style. Kneaded into dough in HBZ style. Wrapped buns in HBZ style. Steamed buns. Dished out buns."
+                , bun.getStepDesc()
+                , "Failure - the output was not 'Mixed stuffing of Pork bun in HBZ style. Kneaded into dough in HBZ style. Wrapped buns in HBZ style. Steamed buns. Dished out buns.'");
+        }
     }
 }
