1970c2c9270c8e8f26c1424a68fb637d56f54238
Ben Wu <wubin28@gmail.com>
2013-12-11 13:43:34 +0800

    In method Cook.MakeBun(), the cook should get the bun from method CreateBun(), prepare stuffing, knead into dough and wrap bun.

--- KataBunStore/GetRawMaterialFromFactories/Cook.cs
+++ KataBunStore/GetRawMaterialFromFactories/Cook.cs
@@ -4,5 +4,13 @@
     {
         protected abstract Bun CreateBun(string bunName);
 
+        public Bun MakeBun(string bunName)
+        {
+            Bun bun = CreateBun(bunName);
+            prepareStuffing(bun, bunName);
+            KneadIntoDough(bun);
+            WrapBun(bun);
+            return bun;
+        }
     }
 }
