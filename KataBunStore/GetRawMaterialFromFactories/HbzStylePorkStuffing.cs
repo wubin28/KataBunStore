a45d214f0394d4e3b91bfc7619588ea96b67b264
Ben Wu <wubin28@gmail.com>
2013-12-12 00:32:24 +0800

    Appended step description in the constructor of class HbzStylePorkStuffing.

--- KataBunStore/GetRawMaterialFromFactories/HbzStylePorkStuffing.cs
+++ KataBunStore/GetRawMaterialFromFactories/HbzStylePorkStuffing.cs
@@ -2,6 +2,10 @@
 {
     public class HbzStylePorkStuffing : Stuffing
     {
+        public HbzStylePorkStuffing()
+        {
+            Desc += "Got mixed stuffing of Pork bun from HBZ Ingredient Factory.";
+        }
         public override string GetDesc()
         {
             return Desc;
