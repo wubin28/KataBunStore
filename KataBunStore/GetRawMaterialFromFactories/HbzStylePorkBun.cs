23d05a10e1e91027558730d81982d2fc06f052fa
Ben Wu <wubin28@gmail.com>
2013-12-12 00:47:39 +0800

    Added class HbzStylePorkBun.

---
+++ KataBunStore/GetRawMaterialFromFactories/HbzStylePorkBun.cs
@@ -0,0 +1,15 @@
+﻿namespace GetRawMaterialFromFactories
+{
+    public class HbzStylePorkBun : Bun
+    {
+        public HbzStylePorkBun(BunIngredientFactory hbzIngredientFactory)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetStepDesc()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
