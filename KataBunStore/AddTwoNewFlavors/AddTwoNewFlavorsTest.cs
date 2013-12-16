0d8c800403496d317ad517c4ed3f03c1fdaf9bec
Ben Wu <wubin28@gmail.com>
2013-12-16 21:05:50 +0800

    Removed unused method and converted method Bun.getStepDesc() to property.

--- KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
+++ KataBunStore/AddTwoNewFlavors/AddTwoNewFlavorsTest.cs
@@ -18,7 +18,7 @@ namespace AddTwoNewFlavors
 
             // Then
             Assert.AreEqual("Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns."
-                , bun.GetStepDesc()
+                , bun.StepDesc
                 , "Failure - the output was not 'Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.'");
         }
 
@@ -33,7 +33,7 @@ namespace AddTwoNewFlavors
 
             // Then
             Assert.AreEqual("Mixed stuffing of Pork bun in GBL style. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns."
-                , bun.GetStepDesc()
+                , bun.StepDesc
                 , "Failure - the output was not 'Mixed stuffing of Pork bun in GBL style. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.'");
         }
 
@@ -48,7 +48,7 @@ namespace AddTwoNewFlavors
 
             // Then
             Assert.AreEqual("Mixed stuffing of Pork bun in HBZ style. Kneaded into dough in HBZ style. Wrapped buns in HBZ style. Steamed buns. Dished out buns."
-                , bun.GetStepDesc()
+                , bun.StepDesc
                 , "Failure - the output was not 'Mixed stuffing of Pork bun in HBZ style. Kneaded into dough in HBZ style. Wrapped buns in HBZ style. Steamed buns. Dished out buns.'");
         }
     }
--- KataBunStore/AddTwoNewFlavors/Bun.cs
+++ KataBunStore/AddTwoNewFlavors/Bun.cs
@@ -4,28 +4,14 @@
     {
         private string stepDesc;
 
-        public string GetStepDesc(string descType)
-        {
-            if (descType == "QfStyleGreenOnionAndPork")
-            {
-                return
-                    "Mixed stuffing of Green Onion and Pork bun in QF style. Kneaded into dough in QF style. Wrapped buns in QF style. Steamed buns. Dished out buns.";
-            }
-            else
-            {
-                return
-                    "Mixed stuffing of Pork bun in GBL style. Kneaded into dough in GBL style. Wrapped buns in GBL style. Steamed buns. Dished out buns.";
-            }
-        }
-
         public void AppendStepDesc(string desc)
         {
             this.stepDesc += desc;
         }
 
-        public string GetStepDesc()
+        public string StepDesc
         {
-            return this.stepDesc;
+            get { return this.stepDesc; }
         }
     }
 }
