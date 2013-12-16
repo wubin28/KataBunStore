0ca1b532da27efd4e47c0f2908679f945ab5373a
Ben Wu <wubin28@gmail.com>
2013-12-16 20:52:06 +0800

    Replaced protected field Bun.StepDesc with protected property.

--- KataBunStore/GetRawMaterialFromFactories/Bun.cs
+++ KataBunStore/GetRawMaterialFromFactories/Bun.cs
@@ -4,8 +4,8 @@
     {
         protected Stuffing Stuffing { get; set; }
         protected Flour Flour { get; set; }
+        protected string StepDesc { get; set; }
 
-        protected string StepDesc = "";
         public abstract string GetStepDesc();
 
         public void AppendStepDesc(string stepDesc)
